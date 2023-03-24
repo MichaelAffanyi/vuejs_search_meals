c08bec2f1f5a2bd57d4d1bb1f9a53163e3471467
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-03-24 14:06:37 +0000

    setup: install vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
-// import './style.css'
+import router from './router/'
+import store from './store/'
 import App from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(router)
+.use(store)
+.mount('#app')
